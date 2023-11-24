f07873e6346c79238099fdf953f889b9dc82068a
heywhyyou <havenoreasontolive@gmail.com>
2023-11-25 01:37:50 +0300

    feat: make favorites work

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const degrees = document.querySelector(".degrees");
 const city = document.querySelector(".footer__city");
 const weatherIcon = document.querySelector(".weather__icon");
 const form = document.querySelector(".search");
+const favs = new Array();
+const heart = document.querySelector(".footer__button");
+const favList = document.querySelector(".fav__list");
 
 const getWeather = (cityName) => {
   let url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
@@ -25,9 +28,80 @@ const getWeather = (cityName) => {
     });
 };
 
+const checkName = (array, searchString) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].name === searchString) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const addToFavs = (event) => {
+  try {
+    if (checkName(favs, city.textContent)) {
+      throw new Error("Город уже в избранном");
+    }
+    favs.push({ name: city.textContent });
+    render(event);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const deleteFromStorage = (name) => {
+  let index = favs.findIndex(function (item) {
+    return item.name === name;
+  });
+
+  if (index !== -1) {
+    favs.splice(index, 1);
+  }
+};
+
+const removeFav = (event) => {
+  deleteFromStorage(event.target.previousElementSibling.textContent);
+
+  event.target.removeEventListener("click", removeFav);
+
+  render(event);
+};
+
+const render = (event) => {
+  let allCities = document.querySelectorAll("li");
+  allCities.forEach((task) => {
+    task.remove();
+  });
+
+  favs.forEach(function (city) {
+    let newName = city.name;
+    let newFav = document.createElement("li");
+    let newP = document.createElement("p");
+    let newButton = document.createElement("button");
+
+    newP.textContent = newName;
+    newButton.classList.add("lnr");
+    newButton.classList.add("lnr-cross");
+    newP.classList.add("fav-city");
+
+    newFav.appendChild(newP);
+    newFav.appendChild(newButton);
+
+    favList.appendChild(newFav);
+
+    newP.addEventListener("click", favHandler);
+    newButton.addEventListener("click", removeFav);
+  });
+};
+
 const submitHandler = (e) => {
   e.preventDefault();
   getWeather(input.value);
 };
 
+const favHandler = (e) => {
+  getWeather(e.target.textContent);
+};
+
 form.addEventListener("submit", submitHandler);
+heart.addEventListener("click", addToFavs);
